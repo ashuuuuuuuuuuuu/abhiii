c30fcf74168a5b655fab5f3cf76e175ed34e37e9
Hnnnkaksnsbssj <99557286+Hnnnkaksnsbssj@users.noreply.github.com>
2022-11-28 21:29:20 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,17 +1,17 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("APP_ID", "6435225"))
-    API_HASH = os.environ.get("API_HASH", "4e984ea35f854762dcde906dce426c2d")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    STRING_SESSION = os.environ.get("STRING_SESSION", "")
+    API_ID = int(os.environ.get("APP_ID", "13893880"))
+    API_HASH = os.environ.get("API_HASH", "16711885d35aa10f66c69031c79c6288")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5842307873:AAHq5CdVQQ-d-zWv7SOD_xpJnsuexfMIlFs")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOHABux7Wwfw0Hbm1VY-xB_BtmEPTlHVTCOH5ILDV2QTa1P136CY3F_Yxi5WrDvVuS_j1C03xY13LDPEvYyXwFH6W_EoQUfXcWXRYodTKdSDXsZWt89f7nlN7OfjKbb3FGQoxynxbTsrUQZibiYyUneJ1n7o2eJrxjfUJwFmgjEE8SSYRfsHi4mmgZhoK6PwTFbHMX022VyX7L3ZzDI_J8DKLFmEdckkWcwWP0Qv-h4GQysCYGTOJoUGOoxEkf7cd-EFRmddwiJEnG5UmjVVoIEiM1OpYuWc-IXkW7HJNhHVLNjouCWm5WwqLuUgUFnGUwllO6aAD0u7tzzvCDGLzOvxbZdA=")
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "ROLEXX_MUSIC_BOT")
     SUPPORT = os.environ.get("SUPPORT", "TheSupportChat") # Your Support
     CHANNEL = os.environ.get("CHANNEL", "TheUpdatesChannel") # Your Channel
-    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png")
-    CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
-    ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "")) # telegram I'd not Username
+    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/47a9c02d48e24fa6a3a62.jpg")
+    CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/47a9c02d48e24fa6a3a62.jpg")
+    ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "5166301441")) # telegram I'd not Username
     AUTO_LEAVE_TIME = int(os.environ.get("AUTO_LEAVE_ASSISTANT_TIME", "54000")) # in seconds
     AUTO_LEAVE = os.environ.get('AUTO_LEAVING_ASSISTANT', None) # Change it to "True"
